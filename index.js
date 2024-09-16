c9d62f68b14e7438f68000a32353b9d56d3ec945
latifamuzaffari <167010074+latifamuzaffari@users.noreply.github.com>
2024-09-16 01:58:27 -0700

    index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "t2c95710o0f6f81ca031b16df4bbe950";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
